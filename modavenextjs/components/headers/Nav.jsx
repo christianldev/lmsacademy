e21a076840feefd2f307eff2c5ce960496b357db
ivette03 <iacascante@est.istg.edu.ec>
2026-07-09 21:57:48 -0500

    Agregando mejora en diseño en el boton de inscribirme

--- modavenextjs/components/headers/Nav.jsx
+++ modavenextjs/components/headers/Nav.jsx
@@ -33,56 +33,10 @@ export default function Nav() {
         } `}
       >
         <a href="#" className="item-link">
-          Home
-          <i className="icon icon-arrow-down" />
+          Inicio
+       
         </a>
-        <div className="sub-menu mega-menu">
-          <div className="container">
-            <div className="row-demo">
-              {demoItems.slice(0, 12).map((item, index) => (
-                <div
-                  className={`demo-item ${
-                    pathname.split("/")[1] === item.href.split("/")[1]
-                      ? "active"
-                      : ""
-                  }`}
-                  key={item.href}
-                >
-                  <Link href={item.href}>
-                    <div className="demo-image position-relative">
-                      <Image
-                        className="lazyload"
-                        data-src={item.src}
-                        alt={item.alt}
-                        src={item.src}
-                        width={273}
-                        height={300}
-                      />
-                      {item.label.length > 0 && (
-                        <div className="demo-label">
-                          {item.label.map((label, labelIndex) => (
-                            <span
-                              key={labelIndex}
-                              className={`demo-${label.toLowerCase()}`}
-                            >
-                              {label}
-                            </span>
-                          ))}
-                        </div>
-                      )}
-                    </div>
-                    <span className="demo-name">{item.name}</span>
-                  </Link>
-                </div>
-              ))}
-            </div>
-            <div className="text-center view-all-demo">
-              <a href="#modalDemo" data-bs-toggle="modal" className="tf-btn">
-                <span className="text">View All Demos</span>
-              </a>
-            </div>
-          </div>
-        </div>
+       
       </li>
       <li
         className={`menu-item ${
@@ -97,122 +51,10 @@ export default function Nav() {
         } `}
       >
         <a href="#" className="item-link">
-          Shop
-          <i className="icon icon-arrow-down" />
+          Clases
+        
         </a>
-        <div className="sub-menu mega-menu">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-2">
-                <div className="mega-menu-item">
-                  <div className="menu-heading">Shop Layout</div>
-                  <ul className="menu-list">
-                    {shopLayout.map((link, index) => (
-                      <li
-                        key={index}
-                        className={`menu-item-li ${
-                          pathname.split("/")[1] == link.href.split("/")[1]
-                            ? "active"
-                            : ""
-                        } `}
-                      >
-                        <Link href={link.href} className="menu-link-text">
-                          {link.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-              <div className="col-lg-2">
-                <div className="mega-menu-item">
-                  <div className="menu-heading">Shop Features</div>
-                  <ul className="menu-list">
-                    {shopFeatures.map((link, index) => (
-                      <li
-                        key={index}
-                        className={`menu-item-li ${
-                          pathname.split("/")[1] == link.href.split("/")[1]
-                            ? "active"
-                            : ""
-                        } `}
-                      >
-                        <Link href={link.href} className="menu-link-text">
-                          {link.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-              <div className="col-lg-2">
-                <div className="mega-menu-item">
-                  <div className="menu-heading">Products Hover</div>
-                  <ul className="menu-list">
-                    {productStyles.map((style, index) => (
-                      <li
-                        key={index}
-                        className={`menu-item-li ${
-                          pathname.split("/")[1] == style.href.split("/")[1]
-                            ? "active"
-                            : ""
-                        } `}
-                      >
-                        <Link href={style.href} className="menu-link-text">
-                          {style.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-              <div className="col-lg-2">
-                <div className="mega-menu-item">
-                  <div className="menu-heading">My Pages</div>
-                  <ul className="menu-list">
-                    {otherShopMenus.map((link, index) => (
-                      <li
-                        key={index}
-                        className={`menu-item-li ${
-                          pathname.split("/")[1] == link.href.split("/")[1]
-                            ? "active"
-                            : ""
-                        } `}
-                      >
-                        <Link href={link.href} className="menu-link-text">
-                          {link.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="wrapper-sub-shop">
-                  <div className="menu-heading">Recent Products</div>
-                  <Swiper
-                    dir="ltr"
-                    className="swiper tf-product-header"
-                    slidesPerView={2}
-                    spaceBetween={20}
-                  >
-                    {products
-                      .slice(0, 4)
-                      .map((elm) => ({
-                        ...elm,
-                        colors: null,
-                      }))
-                      .map((elm, i) => (
-                        <SwiperSlide key={i} className="swiper-slide">
-                          <ProductCard1 product={elm} />
-                        </SwiperSlide>
-                      ))}
-                  </Swiper>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+       
       </li>
       <li
         className={`menu-item ${
@@ -224,129 +66,10 @@ export default function Nav() {
         } `}
       >
         <a href="#" className="item-link">
-          Products
-          <i className="icon icon-arrow-down" />
+          Testimonios
+         
         </a>
-        <div className="sub-menu mega-menu">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-3">
-                <div className="mega-menu-item">
-                  <div className="menu-heading">Products Layout</div>
-                  <ul className="menu-list">
-                    {productLinks.map((link, index) => (
-                      <li
-                        key={index}
-                        className={`menu-item-li ${
-                          pathname.split("/")[1] == link.href.split("/")[1]
-                            ? "active"
-                            : ""
-                        } `}
-                      >
-                        <Link href={link.href} className="menu-link-text">
-                          {link.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-              <div className="col-lg-3">
-                <div className="mega-menu-item">
-                  <div className="menu-heading">Colors Swatched</div>
-                  <ul className="menu-list">
-                    {swatchLinks.map((link, index) => (
-                      <li
-                        key={index}
-                        className={`menu-item-li ${
-                          pathname.split("/")[1] == link.href.split("/")[1]
-                            ? "active"
-                            : ""
-                        } `}
-                      >
-                        <Link href={link.href} className="menu-link-text">
-                          {link.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-              <div className="col-lg-3">
-                <div className="mega-menu-item">
-                  <div className="menu-heading">Products Features</div>
-                  <ul className="menu-list">
-                    {productFeatures.map((link, index) => (
-                      <li
-                        key={index}
-                        className={`menu-item-li ${
-                          pathname.split("/")[1] == link.href.split("/")[1]
-                            ? "active"
-                            : ""
-                        } `}
-                      >
-                        <Link
-                          href={link.href}
-                          className={`menu-link-text ${
-                            link.badge ? "position-relative" : ""
-                          } `}
-                        >
-                          {link.name}
-                          {link.badge && (
-                            <div className="demo-label">
-                              <span className="demo-new">{link.badge}</span>
-                            </div>
-                          )}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-              <div className="col-lg-3">
-                <div className="menu-heading">Best seller</div>
-                <div className="sec-cls-header">
-                  <div className="collection-position hover-img">
-                    <Link href={`/shop-collection`} className="img-style">
-                      <Image
-                        className="lazyload"
-                        data-src="/images/collections/cls-header.jpg"
-                        alt="banner-cls"
-                        src="/images/collections/cls-header.jpg"
-                        width={300}
-                        height={400}
-                      />
-                    </Link>
-                    <div className="content">
-                      <div className="title-top">
-                        <h4 className="title">
-                          <Link
-                            href={`/shop-collection`}
-                            className="link text-white wow fadeInUp"
-                          >
-                            Shop our top picks
-                          </Link>
-                        </h4>
-                        <p className="desc text-white wow fadeInUp">
-                          Reserved for special occasions
-                        </p>
-                      </div>
-                      <div>
-                        <Link
-                          href={`/shop-collection`}
-                          className="tf-btn btn-md btn-white"
-                        >
-                          <span className="text">Shop Now</span>
-                          <i className="icon icon-arrowUpRight" />
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+       
       </li>
       <li
         className={`menu-item position-relative ${
@@ -358,27 +81,10 @@ export default function Nav() {
         } `}
       >
         <a href="#" className="item-link">
-          Blog
-          <i className="icon icon-arrow-down" />
+         Nosotros
+      
         </a>
-        <div className="sub-menu submenu-default">
-          <ul className="menu-list">
-            {blogLinks.map((link, index) => (
-              <li
-                key={index}
-                className={`menu-item-li ${
-                  pathname.split("/")[1] == link.href.split("/")[1]
-                    ? "active"
-                    : ""
-                } `}
-              >
-                <Link href={link.href} className="menu-link-text">
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+        
       </li>
       <li
         className={`menu-item position-relative ${
@@ -390,31 +96,22 @@ export default function Nav() {
         } `}
       >
         <a href="#" className="item-link">
-          Pages
-          <i className="icon icon-arrow-down" />
+         Contacto
+         
         </a>
-        <div className="sub-menu submenu-default">
-          <ul className="menu-list">
-            {otherPageLinks.map((link, index) => (
-              <li
-                key={index}
-                className={`menu-item-li ${
-                  pathname.split("/")[1] == link.href.split("/")[1]
-                    ? "active"
-                    : ""
-                } `}
-              >
-                <Link href={link.href} className="menu-link-text">
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+        
       </li>
       <li className="menu-item">
         <a href="https://themeforest.net/user/themesflat" className="item-link">
-          Buy Theme
+          Pago en línea
+        </a>
+      </li>
+
+
+
+      <li className="btn-inscribirme">
+        <a href="#" >
+        Inscribirme 
         </a>
       </li>
     </>
